8f54c418546600c7ee0d8585eb933b2aa7b6488a
ArpitRathi1 <rathi844@gmail.com>
2024-12-16 15:28:04 +0530

    update

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,25 +1,33 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
 
-  const [products, setProducts] = useState([])
-
-  const fetchData = async() => {
+  const fetchData = async () => {
     const data = await fetch("https://dummyjson.com/products?limit=100");
-    const jsonData = await data.json()
-    console.log(jsonData)
-    setProducts(jsonData?.products)
-    console.log(products)
-  }
+    const jsonData = await data.json();
+    setProducts(jsonData?.products);
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      
+    <div className="w-full">
+      {products.length > 0 && (
+        <div className="m-5 grid grid-cols-3 mx-auto w-[90%]">
+          {products.map((product) => {
+            return (
+              <div key={product.id} className="flex justify-center items-center flex-col shadow-lg w-[80%] my-4">
+                <img src={product.thumbnail} alt="Image"></img>
+                <h3>{product.title}</h3>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
