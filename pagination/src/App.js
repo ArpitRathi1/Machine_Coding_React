856ddb473663bd6dd178389901996527c57a9b4b
ArpitRathi1 <rathi844@gmail.com>
2024-12-16 15:02:36 +0530

    update pagination

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  const fetchData = async() => {
+    const data = await fetch("https://dummyjson.com/products?limit=100");
+    const jsonData = await data.json()
+    console.log(jsonData)
+    setProducts(jsonData?.products)
+    console.log(products)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
